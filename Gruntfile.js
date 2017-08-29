12ebceb82d4b48e996b784bef93a3598e9bc5319
Kaelinator <kirkkael@gmail.com>
2017-08-28 23:27:56 -0500

    Boilerplate code & file structure

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		express: {
+			dev: {
+				options: {
+					script: 'main.js'
+				}
+			}
+		},
+
+		watch: {
+
+			options: {
+				livereload: true
+			},
+
+			express: {
+				files: [
+					'front/source/**/*.handlebars',
+					'front/source/**/*.js',
+					'back/**/*.js',
+					'main.js'
+				],
+				tasks: ['reboot'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-express-server');
+
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('reboot', ['express:dev:stop', 'express:dev']);
+};
