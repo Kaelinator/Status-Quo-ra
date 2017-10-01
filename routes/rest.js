fab3b78d8ef0935a1666ac8515b224ea0bebb9d1
Kaelinator <kirkkael@gmail.com>
2017-10-01 13:53:45 -0500

    Implemented @justml's suggestions

--- routes/rest.js
+++ routes/rest.js
@@ -1,15 +1,16 @@
 const router = module.exports = require('express').Router()
 const {profiles} = require('../database/models.js')
+const _ = require('lodash')
 
 router.route('/')
   .get(grab)
 
 function grab(req, res, next) {
   
-  if (req.query.query == '')
+  if (req.query.query == '' || req.query.length > 64)
     return res.send([])
 
-  profiles.find({ user: { $regex: RegExp(`.*${req.query.query}.*`, 'i') } })
+  profiles.find({ user: { $regex: RegExp(`.*${_.escapeRegExp(req.query.query)}.*`, 'i') } })
     .then((docs) => { res.send(docs) })
-    .catch((err) => { throw new Error(err) })
+    .catch((err) => { res.status(500).send(err) })
 }
